9688aaa5e93039a5bab52fe7086e77f0e99395e4
Mr. Toni Vanhala <toni.vanhala@vincit.fi>
2017-03-24 20:05:10 +0200

    Search track from Audioscrobbler API

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [reagent "0.6.0"]
-                 [re-frame "0.9.2"]]
+                 [re-frame "0.9.2"]
+                 [day8.re-frame/http-fx "0.1.3"]
+                 [cljs-ajax "0.5.8"]
+                 [com.cemerick/url "0.1.1"]]
 
   :plugins [[lein-cljsbuild "1.1.4"]]
 
--- src/cljs/clojure_ass/events.cljs
+++ src/cljs/clojure_ass/events.cljs
@@ -1,6 +1,23 @@
 (ns clojure-ass.events
     (:require [re-frame.core :as re-frame]
-              [clojure-ass.db :as db]))
+              [clojure-ass.db :as db]
+              [day8.re-frame.http-fx]
+              [ajax.core :as ajax]
+              [cemerick.url :refer [url-encode]]
+              [clojure-ass.api-key :refer [API-KEY]]))
+
+(def BASE-URI (str "http://ws.audioscrobbler.com/2.0/?format=json&api_key=" API-KEY))
+
+(defn mk-search-uri [term]
+  (str BASE-URI "&method=track.search&track=" (url-encode term)))
+
+(defn mk-get-request [uri success-evt fail-evt]
+  {:method            :get
+   :uri               uri
+   :timeout           8000
+   :response-format   (ajax/json-response-format {:keywords? true})
+   :on-success        [success-evt]
+   :on-failure        [fail-evt]})
 
 (re-frame/reg-event-db
  :initialize-db
@@ -16,4 +33,14 @@
   :new-search
   (fn [{:keys [db]} _]
     (let [search-term (:name db)]
-      {:db (update db :history #(conj % search-term))})))
+      {:db (update db :history #(conj % search-term))
+       :http-xhrio (mk-get-request (mk-search-uri search-term)
+                                   :search-success
+                                   :search-fail)})))
+
+(re-frame/reg-event-db
+  :search-success
+  (fn [db [_ result]]
+    (assoc db :search-results (get-in result [:results
+                                              :trackmatches
+                                              :track] []))))
